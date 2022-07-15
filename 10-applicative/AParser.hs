1bab60e25ccaf3c59be176aed0913162214afd22
Junjie Wu <clatisus@gmail.com>
2022-07-15 14:44:50 +0800

    finish 10-applicative hw

--- 10-applicative/AParser.hs
+++ 10-applicative/AParser.hs
@@ -1,12 +1,12 @@
 {- CIS 194 HW 10
    due Monday, 1 April
 -}
+{-# OPTIONS_GHC -Wall #-}
 
 module AParser where
 
-import           Control.Applicative
-
 import           Data.Char
+import Control.Applicative (Alternative(..))
 
 -- A parser for a value of type a is a function which takes a String
 -- represnting the input to be parsed, and succeeds or fails; if it
@@ -57,3 +57,73 @@ posInt = Parser f
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Exercise 1
+
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (a, c) = (f a, c)
+
+instance Functor Parser where
+  fmap f (Parser r) = Parser r'
+    where r' x = first f <$> r x
+
+--  fmap f (Parser r) = Parser (fmap (first f) . r)
+-- first f :: (a, c) -> (b, c)
+-- fmap (first f) :: Maybe (a, c) -> Maybe (b, c)
+
+-------------------------------------------------------------------------------
+-- Exercise 2
+
+instance Applicative Parser where
+  pure x = Parser $ Just . (,) x
+  p1 <*> p2 = Parser p
+    where p x = case runParser p1 x of
+            Nothing -> Nothing
+            Just (p1', rest) -> runParser (fmap p1' p2) rest
+
+-------------------------------------------------------------------------------
+-- Exercise 3
+
+abParser :: Parser (Char, Char)
+abParser = (,) <$> char 'a' <*> char 'b'
+-- λ> runParser abParser "abcdef"
+-- Just (('a','b'),"cdef")
+-- λ> runParser abParser "aebcdf"
+-- Nothing
+
+abParser_ :: Parser ()
+-- abParser_ = () <$ char 'a' <* char 'b'
+abParser_ = () <$ abParser
+
+-- λ> runParser abParser_ "abcdef"
+-- Just ((),"cdef")
+-- λ> runParser abParser_ "aebcdf"
+-- Nothing
+
+intPair :: Parser [Integer]
+-- intPair = (:) <$> posInt <* char ' ' <*> ((:[]) <$> posInt)
+intPair = (\a _ b -> [a, b]) <$> posInt <*> char ' ' <*> posInt
+
+-- λ> runParser intPair "12 34"
+-- Just ([12,34],"")
+
+-------------------------------------------------------------------------------
+-- Exercise 4
+
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  (Parser rp1) <|> (Parser rp2)= Parser $ \x -> rp1 x <|> rp2 x
+
+-------------------------------------------------------------------------------
+-- Exercise 5
+
+intOrUpperCase :: Parser ()
+intOrUpperCase = (() <$ posInt) <|> (() <$ satisfy isUpper)
+
+-- λ> runParser intOrUpperCase "342abcd"
+-- Just ((),"abcd")
+-- λ> runParser intOrUpperCase "XYZ"
+-- Just ((),"YZ")
+-- λ> runParser intOrUpperCase "foo"
+-- Nothing
